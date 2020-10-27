4e9030b32b4d92dd7afe31af55f9fe5a08624559
seokjh93 <68522428+seokjh93@users.noreply.github.com>
2020-10-27 18:19:00 +0900

    현재까지 한거 전부 커밋

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 (function($){
 
 
-    $('#mobile').on('click','#aside a', function(e){
+    $('#mobile').on('click','#aside a, .click-menu-box .click-menu-nav a', function(e){
         e.preventDefault()
         var url = $(this).attr('href')
         $('#container-box > .container').remove()
         $('#container-box').load(url + ' .container')
+    })
 
+    // 클릭메뉴에 메뉴 누르면 메뉴창 사라지기
+    $('#header .click-menu-box a').on('click', function(e){
+        e.preventDefault()
+        $(this).parents('.click-menu-box').hide()
     })
     
     // 하단 메뉴의 햄버거 버튼 누르면 좌측에서 메뉴창 열리기
